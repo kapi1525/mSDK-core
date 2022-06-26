a5e462233347beed4d4849149c39874b6132d905
kapigames <kapigamesdev@gmail.com>
2022-06-27 00:18:23 +0200

    Add mSDK.h

---
+++ inc/mSDK.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+// Fancier than WINAPI
+#define FusionAPI __stdcall
+
+// stollen from: https://github.com/SortaCore/MMF2Exts/blob/0540807e8c84785f2d46e2a60dceea735fcad83f/DarkEdif/Inc/Windows/WindowsDefines.hpp#L12
+#define MFXExport comment(linker, "/EXPORT:" __FUNCTION__ "=" __FUNCDNAME__)
