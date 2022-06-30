bd84fe75b4a44f52442080c50504c9ef8a3e055e
kapigames <kapigamesdev@gmail.com>
2022-06-30 20:20:16 +0200

    Add some comment

--- inc/mSDK.h
+++ inc/mSDK.h
@@ -5,4 +5,5 @@
 #define FusionAPI __stdcall
 
 // stollen from: https://github.com/SortaCore/MMF2Exts/blob/0540807e8c84785f2d46e2a60dceea735fcad83f/DarkEdif/Inc/Windows/WindowsDefines.hpp#L12
+// Repleaces .def file and allows us to save more space
 #define MFXExport comment(linker, "/EXPORT:" __FUNCTION__ "=" __FUNCDNAME__)
