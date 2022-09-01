58f5c7805185a425dffcb4eb5e72cfcc602b5f7f
kapigames <kapigamesdev@gmail.com>
2022-09-01 03:57:58 +0200

    This isnt necesary

--- inc/CommonSDK.hpp
+++ inc/CommonSDK.hpp
@@ -5,15 +5,11 @@
 
 
 // Fancier than WINAPI
-#ifndef FusionAPI
 #define FusionAPI __stdcall
-#endif
 
 // stollen from: https://github.com/SortaCore/MMF2Exts/blob/0540807e8c84785f2d46e2a60dceea735fcad83f/DarkEdif/Inc/Windows/WindowsDefines.hpp#L12
 // Repleaces .def file and allows us to save more space
-#ifndef MFXExport
 #define MFXExport comment(linker, "/EXPORT:" __FUNCTION__ "=" __FUNCDNAME__)
-#endif
 
 
 #define DllImport __declspec(dllimport)
